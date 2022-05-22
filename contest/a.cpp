b79d2b94981d655e9fdae959117bd3db00ae87c2
Raiyan Hossain <raiyan15-10258@diu.edu.bd>
2022-05-22 12:54:47 +0600

    solved a 1500 rating problem in codeforces

--- contest/a.cpp
+++ contest/a.cpp
@@ -1,25 +1,35 @@
-/**
- here we can simply sort the array and print the higest kth element.
- time complexity = O(Nlog2(N))
-
- I know another solution which is similar to quick sort
- and the complexity will be O(N)
- but do not enough time to implement that.
- * */
-
 #include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-   
- int n,k;
- scanf("%d %d",&n,&k);
- vector<int>v(n);
- for(int i=0;i<n;i++){
-     scanf("%d",&v[i]);
- }
- sort(v.begin(),v.end());
- printf("%d\n",v[n-k]);
- 
+    int n;
+    scanf("%d",&n);
+    map<int,int>mf,mb;
+    for(int i=0;i<n;i++){
+        int f,b;
+        scanf("%d %d",&f,&b);
+        mb[f]=b;
+        mf[b]=f;
+    } 
+    vector<int>v(n);
+    int front=0;
+    for(int i=1;i<n;i+=2){
+        v[i]=mb[front];
+        front=v[i];
+    }
+    int firstNumber;
+    for(auto it=mb.begin();it!=mb.end();it++){
+        if( mf[it->first]==0&& it->first!=v[1]){
+            firstNumber=it->first;
+        }
+    }
+    v[0]=firstNumber;
+    for(int i=2;i<n;i+=2){
+        v[i]=mb[v[i-2]];
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",v[i]);
+    }
+    printf("\n");
 }
